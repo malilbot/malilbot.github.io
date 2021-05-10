afa42fd28338907f0036b23211605ac74e35770c
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-10 18:30:35 +0200

    Fix invite

--- src/pages/invite.js
+++ src/pages/invite.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-class Contact extends React.Component {
-    componentDidMount() { window.location.href = "https://discord.com/oauth2/authorize?client_id=749020331187896410&permissions=117824&scope=bot%20applications.commands"; }
-    render() {
-        return (
-
-            <div className="container">
-                If the redirect didnt work click [this](https://discord.com/oauth2/authorize?client_id=749020331187896410&permissions=117824&scope=bot%20applications.commands)
-            </div>
-
-        )
-    }
+class Invite extends React.Component {
+	componentDidMount() {
+		window.location.href =
+			'https://discord.com/oauth2/authorize?client_id=749020331187896410&permissions=117824&scope=bot%20applications.commands';
+	}
+	render() {
+		return (
+			<div>
+				If the redirect didnt work click this: https://discord.com/oauth2/authorize?client_id=749020331187896410&permissions=117824&scope=bot%20applications.commands
+			</div>
+		);
+	}
 }
 
-export default Contact;
+export default Invite;
