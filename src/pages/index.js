b1416b65827771b93374c6bee858cb0b06557ada
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-20 18:38:30 +0200

    Snaking updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,50 +4,47 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import HomepageFeatures from '../css/HomepageFeatures';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx('hero hero--primary', styles.heroBanner)}>
+			<div className="container">
+				<h1 className="hero__title">{siteConfig.title}</h1>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
 
-        <div className={styles.buttons}>
-          <p>
-            <Link
-              className="buttonWrapper button button--secondary button--lg"
-              to="/docs/intro">
-              Malil documentation
-            </Link>
-          </p>
-          <p>
-
-          </p>
-          <p>
-            <Link
-              className="button button--secondary button--lg"
-              to="/invite">
-              Invite Malil
-            </Link>
-          </p>
-
-        </div>
-      </div>
-    </header>
-  );
+				<div className={styles.buttons}>
+					<p>
+						<Link
+							className="buttonWrapper button button--secondary button--lg"
+							to="/docs/intro"
+						>
+							Malil documentation
+						</Link>
+					</p>
+					<p></p>
+					<p>
+						<Link className="button button--secondary button--lg" to="/invite">
+							Invite Malil
+						</Link>
+					</p>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home() {
-  return (
-    <Layout
-      title={`Home page`}
-      description="Malil the all in one discord bot <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	return (
+		<Layout
+			title={`Home page`}
+			description="Malil the all in one discord bot <head />"
+		>
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
 }
